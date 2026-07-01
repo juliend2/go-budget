058d18a50fef872432e93e5bf41f5f46ec353192
Julien Desrosiers <juliend2@gmail.com>
2026-07-01 15:29:10 -0400

    test more GetPayDays()

--- repository/expense_test.go
+++ repository/expense_test.go
@@ -13,15 +13,15 @@ func TestGetPayDays(t *testing.T) {
 	// Arrange
 	dateRange := model.DateRange{
 		From: model.Date(2025, time.December, 1),
-		To:   model.Date(2025, time.December, 31),
+		To:   model.Date(2026, time.January, 15),
 	}
 
 	// Act
 	payDays := repository.GetPayDays(dateRange)
 
 	// Assert
-	if len(payDays) != 2 {
-		//t.Errorf("len(GetPayDays()) = %d; want 2", len(payDays))
+	if len(payDays) != 3 {
+		t.Errorf("len(GetPayDays()) = %d; want 3", len(payDays))
 	}
 
 	got := carbon.NewCarbon(payDays[0]).ToDateString()
@@ -34,4 +34,8 @@ func TestGetPayDays(t *testing.T) {
 		t.Errorf("payDays[1] = %s; want 2025-12-31", got)
 	}
 
+	got = carbon.NewCarbon(payDays[2]).ToDateString()
+	if got != "2026-01-15" {
+		t.Errorf("payDays[2] = %s; want 2026-01-15", got)
+	}
 }
