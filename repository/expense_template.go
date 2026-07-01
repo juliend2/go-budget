42c3bb148cf5a968802afa11e91f367339cdb695
Julien Desrosiers <juliend2@gmail.com>
2026-06-30 20:51:34 -0400

    Test: expense was after the template's InitialToBePaidOn

--- repository/expense_template.go
+++ repository/expense_template.go
@@ -12,19 +12,26 @@ func GenerateRepeatingExpenses(expTpl *model.ExpenseTemplate, dateRange model.Da
 	expenses := []*model.Expense{}
 	cTime := carbon.NewCarbon(expTpl.InitialToBePaidOn)
 
+	// Here, we don't want to also do cTime.Gte(carbon.NewCarbon(dateRange.From))
+	// because if we do, we never start the loop since the condition is never met
+	// when the InitialToBePaidOn happened before the dateRange.From.
+	// And we're not concerned about filtering out paid expenses just yet, so if
+	// there are past unpaid expenses, we do want them in those results.
 	for cTime.Lt(carbon.NewCarbon(dateRange.To)) {
+		newTime := cTime
 		switch pace := expTpl.RepeatabilityIntervalPace; pace {
 		case "D":
-			cTime = cTime.AddDays(expTpl.RepeatabilityIntervalUnit)
+			newTime = cTime.AddDays(expTpl.RepeatabilityIntervalUnit)
 		case "W":
-			cTime = cTime.AddWeeks(expTpl.RepeatabilityIntervalUnit)
+			newTime = cTime.AddWeeks(expTpl.RepeatabilityIntervalUnit)
 		case "M":
-			cTime = cTime.AddMonths(expTpl.RepeatabilityIntervalUnit)
+			newTime = cTime.AddMonths(expTpl.RepeatabilityIntervalUnit)
 		case "Y":
-			cTime = cTime.AddYears(expTpl.RepeatabilityIntervalUnit)
+			newTime = cTime.AddYears(expTpl.RepeatabilityIntervalUnit)
 		default:
 			return []*model.Expense{}, errors.New(fmt.Sprintf("Time interval pace '%s' not supported", pace))
 		}
+		cTime = newTime
 		expenses = append(expenses, model.NewExpense(expTpl.Amount, cTime.StdTime()))
 	}
 
--- repository/expense_template_test.go
+++ repository/expense_template_test.go
@@ -8,6 +8,30 @@ import (
 	"desrosiers.org/budget/repository"
 )
 
+func TestExpenseTemplateRepetitionCreatesExpenseForTemplateStartingBeforeFrom(t *testing.T) {
+	// Arrange
+	expTpl := model.NewExpenseTemplate(
+		100,
+		"abonnement",
+		model.WithInitialToBePaidOn(2026, time.May, 1),
+		model.WithRepeatabilityInterval(1, "M"),
+	)
+
+	// Act
+	expenses, _ := repository.GenerateRepeatingExpenses(
+		expTpl,
+		model.DateRange{
+			From: model.Date(2026, time.June, 1),
+			To:   model.Date(2026, time.June, 2), // excludes the `To`
+		},
+	)
+
+	// Assert
+	if len(expenses) != 2 {
+		t.Errorf("len(GenerateRepeatingExpenses()) = %d; want 2", len(expenses))
+	}
+}
+
 func TestExpenseTemplateRepetitionCreatesFirstTwoOccurrences(t *testing.T) {
 	// Arrange
 	expTpl := model.NewExpenseTemplate(
@@ -21,7 +45,7 @@ func TestExpenseTemplateRepetitionCreatesFirstTwoOccurrences(t *testing.T) {
 	expenses, _ := repository.GenerateRepeatingExpenses(
 		expTpl,
 		model.DateRange{
-			From: model.Date(2026, time.June, 2),
+			From: model.Date(2026, time.May, 29),
 			To:   model.Date(2026, time.July, 29),
 		},
 	)
