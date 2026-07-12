d3efbc8f1af5773a419e45efa7ca1f02675898b1
Julien Desrosiers <juliend2@gmail.com>
2026-07-11 21:04:43 -0400

    use dynamic dates

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 
 	"desrosiers.org/budget/model"
 	"desrosiers.org/budget/repository"
+	"github.com/dromara/carbon/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -94,11 +95,9 @@ func handleDashboard(repo *repository.MongoDBRepository, tmpl *template.Template
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 
-		// Dashboard date range: from the start of current period (2026-06-30)
-		// up to 1 month in advance (2026-08-15)
 		dateRange := model.DateRange{
-			From: model.Date(2026, 6, 30), // TODO: make ths dynamic
-			To:   model.Date(2026, 8, 15), // TODO: make ths dynamic
+			From: model.GetPreviousPayDate(time.Now()),
+			To:   model.GetNextPayDate(carbon.NewCarbon(time.Now()).AddMonth().StdTime()),
 		}
 
 		// 1. Generate/Ensure repeating expenses for active templates in the range
--- model/date.go
+++ model/date.go
@@ -1,7 +1,30 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/dromara/carbon/v2"
+)
 
 func Date(y int, m time.Month, d int) time.Time {
 	return time.Date(y, m, d, 0, 0, 0, 0, time.UTC)
+}
+
+func GetPreviousPayDate(t time.Time) time.Time {
+	d := carbon.NewCarbon(t)
+	if d.Day() > 15 {
+		return d.SetDay(15).StdTime()
+	} else {
+		// past month's last day:
+		return d.StartOfMonth().SubDay().StdTime()
+	}
+}
+
+func GetNextPayDate(t time.Time) time.Time {
+	d := carbon.NewCarbon(t)
+	if d.Day() > 15 {
+		return d.EndOfMonth().StdTime()
+	} else {
+		return d.SetDay(15).StdTime()
+	}
 }
